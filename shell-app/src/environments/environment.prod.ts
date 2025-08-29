a49541ec59bc035310e67e9e37b046d19a34ee39
Andon Barhani <BarhanAn@basfad.basf.net>
2025-08-29 11:57:52 -0400

    fix1

--- shell-app/src/environments/environment.prod.ts
+++ shell-app/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  mfe1URL: 'http://...',
+  // If you're not actually loading a remote MFE in production yet,
+  // leave this empty so the app can load without errors
+  mfe1URL: '',
 };
